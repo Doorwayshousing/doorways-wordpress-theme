6416dea65bd2c33de7fc1e8d3abdbcfe42dd1b2d
weffelms <stacie.weffelmeyer@wwt.com>
2016-10-21 12:26:30 -0500

    styling changes

--- donation_page_template.php
+++ donation_page_template.php
@@ -27,8 +27,39 @@ Template Name: Donation Page
 
  <div class="wrapper text-center">
      <div id="customFormWrapper">
+         <div id="donationHeader">
+             <div class="donate-header-text">donate</div>
+             <div class="donate-paragraph-text">You can help. We encourage you to give whatever is within your means. DOORWAYS provides direct assistance to nearly 3,000 people anually through a remarkable continuum of care. Your taxable donation will go a long way toward helping us provide safe, affordable housing for people living with and affected by HIV/AIDS. Please help us open the doors to more clients. <i>DOORWAYS, an IRC 501(c)3 tax-exempt Missouri nonprofit corporation, is registered to solicit funds for charitable purposes in Missouri and Illinois. DOORWAYS only solicits such charitable contributions in MO and IL.</i>
+             </div>
+         </div>
+         <div id="donation-breakdown">
+             <div class="breakdown-text">How will my donation help?</div>
+             <div class="percentage-section">
+                 <div class="percent-section-breakdown">housing</div>
+                 <div class="percent-section-breakdown">food</div>
+                 <div class="percent-section-breakdown">supplies</div>
+             </div>
+             <div class="breakdown-text-2">100% Tax-deductible gift contributes directly to the compassionate care of hundres of adults and children living with HIV/AIDS.</div>
+         </div>
          <form class="form" id="myCustomForm">
-             <div class="form-group billing" id="name-group">
+             <div class="form-header">DONATE</div>
+             <div class="form-group donation" id="donation-level">
+                 <select class="donation-dropdown" name="donation-level">
+                     <option selected disabled>Choose Donation Level</option>
+                     <option value="friends">Friends - One Time Donation</option>
+                     <option value="keystoneSociety">Keystone Society - $1,200 +</option>
+                     <option value="keystoneLegacy">Keystone Legacy - planned legacy gift</option>
+                 </select>
+             </div>
+             <div class="form-group donation" id="donation-amount">
+                 <input type="text" class="form-control" id="donation-amount" value="" placeholder="$ USD">
+             </div>
+             <button class="paymentButton" id="payOnceButton">ONCE</button>
+             <button class="paymentButton" id="payMonthlyButton">MONTHLY</button>
+             <div class="other-payment-options">PAY BY PERESONAL CHECK <br/>PAY BY BUSINESS CHECK
+             </div>
+
+             <!-- <div class="form-group billing" id="name-group">
                  <label class="control-label">Name</label>
                  <input type="text" class="form-control" id="billing_name" value="" placeholder="">
                  <span class="help-block"></span>
@@ -69,7 +100,7 @@ Template Name: Donation Page
                  <span class="help-block"></span>
              </div>
 
-             <button class="btn btn-primary" id="paymentButton">Pay Now</button>
+             <button class="btn btn-primary" id="paymentButton">Pay Now</button> -->
          </form>
          <br />
          <h5>Results:</h5>
@@ -77,6 +108,6 @@ Template Name: Donation Page
      </div>
      <div id="paymentResponse" class="alert alert-success" role="alert"></div>
  </div>
- <br /><br /><br />
+
 
  <?php get_footer(); ?>
