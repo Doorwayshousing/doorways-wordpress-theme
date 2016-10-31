92db34648b22253c0e0d1e6a3189006f7e6c239c
kennedyb <brian.kennedy@wwt.com>
2016-10-31 13:03:02 -0500

    adding

--- home_page_template.php
+++ home_page_template.php
@@ -13,5 +13,39 @@ Template Name: Doorways Home Page
          <i class="fa fa-angle-double-down bounce"></i>
      </div>
  </div>
+ <div class="ne-container">
+     <div class="ne-header">
+         <div class="news-icon">
+             <i class="fa fa-newspaper-o"></i>
+         </div>
+         <div class="events-icon">
+             <i class="fa fa-calendar"></i>
+         </div>
+         <div class="ne-title">
+             News & Events
+         </div>
+     </div>
+     <div class="ne-body">
+         <ul>
+                 <?php
+            $args = array( 'posts_per_page' => 3, 'category_name' => 'News,Events' );
+            $myposts = get_posts( $args );
+            foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+                 <li style="background: url(<?php the_post_thumbnail_url('medium');?>)no-repeat center center; background-size:cover;">
+                     <a href="<?php the_permalink(); ?>">
+                <div class="full-post-link"></div>
+                 </a>
+                    <div class="category-holder">
+                      <div><?php the_title(); ?></div>
+                      <?php the_content(); ?>
+                    </div>
+                  </a>
+                 </li>
+             <?php endforeach;
+         wp_reset_postdata();?>
+     </ul>
+     </div>
+
+ </div>
 
  <?php get_footer(); ?>
