8e1938514cf372f2807aa9cfac2900760cbf0906
weffelms <stacie.weffelmeyer@wwt.com>
2016-11-02 12:40:43 -0500

    social icons to footer

--- footer.php
+++ footer.php
@@ -4,7 +4,10 @@
 </div>
 <div id="footer-bar">
     <div class="copyright">&copy; COPYRIGHT, 2016</div>
-    <div class="social">FOLLOW OUR JOURNEY</div>
+    <div class="social">FOLLOW OUR JOURNEY
+        <div class="facebook"><a href="https://www.facebook.com/DoorwaysAIDSHousing" target="_blank"><i class="fa fa-facebook"></i></a></div>
+        <div class="twitter"><a href="https://twitter.com/Doorways" target="_blank"><i class="fa fa-twitter"></i></a></div>
+    </div>
 </div>
 
 
