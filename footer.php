120534ee2bf6d397c1a3b48d92b950109afe65bd
weffelms <stacie.weffelmeyer@wwt.com>
2016-11-02 12:21:37 -0500

    start of footer content

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
 <? wp_footer();?>
+<div id="sponsor-section">
+    <div class="sponsor">sponsor 1</div>
+</div>
+<div id="footer-bar">
+    <div class="copyright">&copy; COPYRIGHT, 2016</div>
+    <div class="social">FOLLOW OUR JOURNEY</div>
+</div>
+
+
 </body>
 </html>
