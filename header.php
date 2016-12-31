9ec547cd2a1826382f754f286041e04e113a5b5f
mitcheda <david.mitchell@wwt.com>
2016-12-30 21:08:18 -0600

    ch ch changes

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <title>Doorways Housing <?php wp_title($sep = '&raquo;', $seplocation = '$right'); ?></title>
 <link rel="icon" type="image/svg+xml" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/favicon.ico">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Libre+Baskerville"> 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script type="text/javascript" src="https://www.sagepayments.net/pay/1.0.0/js/pay.min.js"></script>
