020cf7d9b4001dad56f3cf1c9159ecc6caed2a6d
mitcheda <david.mitchell@wwt.com>
2017-03-22 22:03:26 -0500

    sub page layout updates

--- sub_page_template.php
+++ sub_page_template.php
@@ -4,17 +4,19 @@ Template Name: Sub Page
 */
  get_header(); ?>
 <div id="page-content-container">
-   <div id="page-content-banner">
-       <div id="banner-title-text">
-           <?php wp_title(''); ?>
-       </div>
-   </div>
-   <div id="page-content-body">
-       <div class="content-text">
-         <?php if (have_posts()) : while (have_posts()) : the_post();?>
-           <?php the_content(); ?>
-         <?php endwhile; endif; ?>
-       </div>
-   </div>
+    <div id="page-content-banner">
+        <div id="banner-title-container">
+            <div id="banner-title-text">
+                <?php wp_title(''); ?>
+            </div>
+        </div>
+    </div>
+    <div id="page-content-body">
+        <div class="content-text">
+            <?php if (have_posts()) : while (have_posts()) : the_post();?>
+                <?php the_content(); ?>
+            <?php endwhile; endif; ?>
+        </div>
+    </div>
 </div>
- <?php get_footer(); ?>
+<?php get_footer(); ?>
